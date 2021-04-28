fa4cf5b026bd2feaeb5c7be9bbd8799dd7c54487
offwoeter <woeter45@gmail.com>
2021-04-28 14:17:42 +0200

    startmenu

---
+++ GIP Sam Michiel/Assets/My assets/Scripts/ExitOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitOnClick : MonoBehaviour
+{
+    //De functie ExitGame zal bij het On Click
+    //event van de QuitButton geconfigureerd worden.
+    public void ExitGame()
+    {
+        //Deze instructie sluit de toepassing af.
+        Application.Quit();
+    }
+}
+
---
+++ GIP Sam Michiel/Assets/My assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Deze extra library is nodig om de instructie
+//LoadScene() te kunnen werken.
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    //Deze functie zullen we in Unity koppelen aan de On Click
+    //event van de Start button. Dit is mogelijk omdat dit een
+    //public functie is. In Unity zullen we eveneens de waarde
+    //voor het argument sceneIndex meegeven.
+    public void StartScene(int sceneIndex)
+    {
+        //De scene met index sceneIndex wordt geladen.
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
+
