307a734d3945960c7c0e5489c07216a92305db40
Sam <woeter45@gmail.com>
2021-05-07 11:44:45 +0200

    Hp bar dmg bar

---
+++ GIP Sam Michiel/Assets/My assets/Scripts/DamageScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour
+{
+    float speed = 200f;
+    public float damage = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        damage = Mathf.Round(Random.Range(10f, 20f));
+        other.gameobject.GetComponent<HealthScript>().TakeDamage(damage);
+    }
+}
---
+++ GIP Sam Michiel/Assets/My assets/Scripts/HealthScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public image healthbar;
+    public float max_health = 100f;
+    public float cur_health = 0f;
+    public bool alive = true;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        alive = true;
+        cur_health = max_health;
+        SetHealthBar ();
+        InvokeRepeating("DoDamage", 1f, 5f);
+    }
+    void DoDamage()
+    {
+        TakeDamage(10f);
+    }
+    public void TakeDamage(float amount)
+    {
+        if (!alive)
+        {
+            return;
+        }
+        if(cur_health <= 0)
+        {
+            cur_health = 0;
+            alive = false;
+        }
+        cur_health -= amount;
+        SetHealthBar ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float my_health = cur_health / max_health;
+        healthbar.transform.localscale = new Vector3(Mathf.clamp(my_health, 0f, 1f), healthbar.transform.localscale.y, healthbar.transform.localscale.x);
+    }
+}
